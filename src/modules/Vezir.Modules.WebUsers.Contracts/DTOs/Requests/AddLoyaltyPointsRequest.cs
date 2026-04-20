03f9e409a023ff22d591e551e4540a374819d5de
Sasa Kitic <sasakiticsanz@gmail.com>
2026-04-20 13:30:25 +0200

    chore(contracts): remove unused request and response DTOs

--- src/modules/Vezir.Modules.WebUsers.Contracts/DTOs/Requests/AddLoyaltyPointsRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Kontrol.Modules.WebUsers.Contracts.DTOs.Requests;
-
-/// <summary>
-/// Request model for adding loyalty points to a web user.
-/// </summary>
-public class AddLoyaltyPointsRequest
-{
-    public int Points { get; init; }
-}
